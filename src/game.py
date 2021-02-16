d564a299a5ad8813f5f705077c882ac2458da9cc
Adrien Funel <funel.adrien@gmail.com>
2021-02-16 14:23:07 +0000

    Liting

--- src/game.py
+++ src/game.py
@@ -198,7 +198,6 @@ class Pipe():
 
         return False
 
-
 class Base:
     """
     Represnts the moving floor of the game
@@ -432,4 +431,3 @@ if __name__ == '__main__':
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config-feedforward.txt')
     run(config_path)
-
